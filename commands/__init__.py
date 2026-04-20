d1f6dab12e54db8fd460ae34b43ff507bdfcce8e
Ferry Prawira Hartono <datahassi@gmail.com>
2026-04-21 02:44:43 +0500

    Add wallet and referral routers to main router

--- commands/__init__.py
+++ commands/__init__.py
@@ -6,6 +6,8 @@ from commands.co import router as co_router
 from commands.proxy import router as proxy_router
 from commands.admin import router as admin_router
 from commands.tempmail import router as temp_router
+from commands.wallet import router as wallet_router
+from commands.referral import router as ref_router
 
 router = Router()
 
@@ -15,3 +17,5 @@ router.include_router(co_router)
 router.include_router(proxy_router)
 router.include_router(admin_router)
 router.include_router(temp_router)
+router.include_router(wallet_router)
+router.include_router(ref_router)
