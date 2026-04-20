d1bdde85002741c1948b27299dd09d35f342ba8c
Ferry Prawira Hartono <datahassi@gmail.com>
2026-04-21 02:00:16 +0500

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,7 +4,6 @@ from commands.start import router as start_router
 from commands.gen import router as gen_router
 from commands.co import router as co_router
 from commands.proxy import router as proxy_router
-from commands.admin import router as admin_router
 
 router = Router()
 
@@ -12,4 +11,3 @@ router.include_router(start_router)
 router.include_router(gen_router)
 router.include_router(co_router)
 router.include_router(proxy_router)
-router.include_router(admin_router)
