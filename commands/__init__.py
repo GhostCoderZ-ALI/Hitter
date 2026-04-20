46b1c90d6a6721ef7f6ae7b3069a20dac9319747
Ferry Prawira Hartono <datahassi@gmail.com>
2026-04-21 01:57:58 +0500

    Remove temp_router from command router
    Removed temp_router from the command router.

--- commands/__init__.py
+++ commands/__init__.py
@@ -5,7 +5,6 @@ from commands.gen import router as gen_router
 from commands.co import router as co_router
 from commands.proxy import router as proxy_router
 from commands.admin import router as admin_router
-from commands.tempmail import router as temp_router
 
 router = Router()
 
@@ -14,4 +13,3 @@ router.include_router(gen_router)
 router.include_router(co_router)
 router.include_router(proxy_router)
 router.include_router(admin_router)
-router.include_router(temp_router)
