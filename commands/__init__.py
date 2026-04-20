b14308c72bfcf7bb6ff7439cbb21aa414b24899a
Ferry Prawira Hartono <datahassi@gmail.com>
2026-04-21 02:27:39 +0500

    Add admin and tempmail routers to the main router

--- commands/__init__.py
+++ commands/__init__.py
@@ -4,6 +4,8 @@ from commands.start import router as start_router
 from commands.gen import router as gen_router
 from commands.co import router as co_router
 from commands.proxy import router as proxy_router
+from commands.admin import router as admin_router
+from commands.tempmail import router as temp_router
 
 router = Router()
 
@@ -11,3 +13,5 @@ router.include_router(start_router)
 router.include_router(gen_router)
 router.include_router(co_router)
 router.include_router(proxy_router)
+router.include_router(admin_router)
+router.include_router(temp_router)
