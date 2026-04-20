a19f860b16bc4f8db487be67cc2250c97fdc9707
Ferry Prawira Hartono <datahassi@gmail.com>
2026-04-21 01:34:51 +0500

    Update __init__.py

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,13 +1,14 @@
 from aiogram import Router
 
-from commands.start  import router as start_router
-from commands.gen    import router as gen_router
-from commands.co     import router as co_router
-from commands.proxy  import router as proxy_router
+from commands.start import router as start_router
+from commands.gen import router as gen_router
+from commands.co import router as co_router
+from commands.proxy import router as proxy_router
 from commands.admin import router as admin_router
 from commands.tempmail import router as temp_router
 
 router = Router()
+
 router.include_router(start_router)
 router.include_router(gen_router)
 router.include_router(co_router)
